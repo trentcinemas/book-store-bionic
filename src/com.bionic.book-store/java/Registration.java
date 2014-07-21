de0978cd62bd72786034960121c69f0779d05fa8
Evgeniy Baranuk <evgeniy.baranuk@gmail.com>
2014-07-21 15:35:28 +0300

    Registration class add

--- src/com.bionic.book-store/java/Registration.java
+++ src/com.bionic.book-store/java/Registration.java
@@ -10,6 +10,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
+import static util.Logger.Type.ERROR;
 import static util.Logger.Type.PROCESS;
 
 /**
@@ -17,21 +18,35 @@ import static util.Logger.Type.PROCESS;
  */
 @Path("register")
 public class Registration {
+    private final String RESPONSE_HEADER = "Access-Control-Allow-Origin";
 
     @POST
     public Response register(@FormParam("email") String email,
                               @FormParam("password") String password,
                               @FormParam("name") String name) {
 
-        DaoUserInterface dao = DaoFactory.getDaoUserInstance();
+        DaoUserInterface daoUser = DaoFactory.getDaoUserInstance();
+
+        // HTTP 409 (Conflict)
+        if (daoUser.exist(email)) {
+            Logger.log(ERROR, email + " already exist");
+            return Response.status(409).header(RESPONSE_HEADER, "*")
+                    .entity("User already exist").build();
+        }
+
+
         User user = new User();
+
         user.setName(name);
         user.setEmail(email);
         user.setPassword(DigestUtils.md5Hex(password));
         // TODO Додати групу юзера
         //  user.setUserGroupByGroupId();
+
         Logger.log(PROCESS, "Registered : " + email);
-        dao.insert(user);
+
+        daoUser.insert(user);
+
         return Response.ok().header("Access-Control-Allow-Origin", "*").build();
     }
 }
