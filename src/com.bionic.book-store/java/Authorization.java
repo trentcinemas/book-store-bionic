95c69d0ccbcfe47d335d0ee1af56a601de32fe90
Evgeniy Baranuk <evgeniy.baranuk@gmail.com>
2014-07-22 17:04:31 +0300

    Add session support with cookies

--- src/com.bionic.book-store/java/Authorization.java
+++ src/com.bionic.book-store/java/Authorization.java
@@ -4,37 +4,68 @@ import org.apache.commons.codec.digest.DigestUtils;
 import util.DaoFactory;
 import util.Logger;
 
-import javax.ws.rs.FormParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
 
 import static util.Logger.Type.PROCESS;
 
 /**
  * Created by Evgeniy Baranuk on 16.07.14.
  */
-@Path("authorize")
+@Path("/")
 public class Authorization {
+    private final String RESPONSE_HEADER = "Access-Control-Allow-Origin";
+
     private DaoUserInterface dao = DaoFactory.getDaoUserInstance();
 
     @POST
-    public Response authorize(@FormParam("email") String email,
+    @Path("login")
+    public Response login(@FormParam("email") String email,
                           @FormParam("password") String password) {
 
         User user = dao.selectByEmail(email);
 
         String hexedPassword = DigestUtils.md5Hex(password);
 
-        if (user != null) {
-            if (hexedPassword.equals(user.getPassword())) {
-                Logger.log(PROCESS, "Authorized : " + email);
-                return Response.ok().header("Access-Control-Allow-Origin", "*").build();
-            }
+        // User not exist
+        if (user == null) {
+            Logger.log(PROCESS, "Not authorized : " + email);
+            return Response.status(400).entity("Неправильні дані авторизації").build();
+        }
+        // Wrong user password
+        if (!hexedPassword.equals(user.getPassword())) {
+            Logger.log(PROCESS, "Invalid password : " + email);
+            return Response.status(400).entity("Неправильні дані авторизації").build();
         }
 
-        Logger.log(PROCESS, "Not authorized : " + email);
-        return Response.status(400).build();
+        // All OK :
+        Logger.log(PROCESS, email + " authorized");
+
+        NewCookie cookie = new NewCookie("user", email);
+        Logger.log(PROCESS, email + " cookie saved");
+
+        return Response.ok()
+                .header(RESPONSE_HEADER, "*").cookie(cookie).build();
+    }
+
+
+    @POST
+    @Path("logout")
+    public Response logout(@CookieParam("user") String user) {
+
+        if (user == null) {
+            Logger.log(PROCESS, "Not login user");
+            return Response.status(400).entity("User not llll").build();
+        }
+
+        Cookie cookie = new Cookie("user", user);
+        NewCookie authorizedCookie =
+                new NewCookie(cookie, "Removed authorized profile cookie", 0, true); // Cookie age = 0
+
+        Logger.log(PROCESS, "Logout : " + user);
+
+        return Response.ok()
+                .header(RESPONSE_HEADER, "*").cookie(authorizedCookie).build();
     }
 
 }
