96e85a37a0f698fde3867b41a8ca38aa70b015cd
Eklierka <vkekler@gmail.com>
2014-07-23 15:25:39 +0300

    dao user test

--- src/test/java/DaoUserTest.java
+++ src/test/java/DaoUserTest.java
@@ -12,7 +12,7 @@ public class DaoUserTest {
     DaoUser daoUser = DaoFactory.getDaoUserInstance();
 
     @Test
-    public void testDaoFactory() throws Exception {
+    public void testSelectAll() throws Exception {
         /*
         list of users isn't empty
          */
@@ -20,18 +20,35 @@ public class DaoUserTest {
         /*
         count of users
          */
-        assertEquals(3, daoUser.selectAll().size());
+        assertEquals(6, daoUser.selectAll().size());
     }
 
     @Test
-    public void testUserInsertion() throws Exception {
+    public void testSelectById() {
+        assertEquals("Kristina", daoUser.selectById(9).getName());
+    }
+
+    @Test
+    public void testSelectByEmail() {
+        assertEquals("mail@mail.ru", daoUser.selectByEmail("mail@mail.ru").getEmail());
+    }
+
+    @Test
+    public void testDeleteById() throws Exception {
+        testInsert();
+        daoUser.delete(5);
+        assertNull(daoUser.selectById(5));
+    }
+
+    @Test
+    public void testInsert() throws Exception {
         /*
         create new user
          */
         User testUser = new User();
         testUser.setName("eklierka");
         testUser.setPassword(DigestUtils.md5Hex("password"));
-        testUser.setEmail("");
+        testUser.setEmail("email@gmail.com");
         testUser.setUserId(5);
         /*
         insert user to the db
@@ -43,17 +60,43 @@ public class DaoUserTest {
         assertTrue(daoUser.selectAll().contains(testUser));
     }
 
-    @Ignore
     @Test
     public void testUserInfo() throws Exception {
         assertEquals(5, daoUser.selectById(5).getUserId());
-        assertEquals("", daoUser.selectById(5).getEmail());
+        assertEquals("email@gmail.com", daoUser.selectById(5).getEmail());
         assertEquals(DigestUtils.md5Hex("password"), daoUser.selectById(5).getPassword());
     }
 
     @Test
-    public void testDeleteUser() throws Exception{
+    public void testDelete() throws Exception{
+        testInsert();
         daoUser.delete(daoUser.selectById(5));
-        assertEquals(null,daoUser.selectById(5));
+        assertEquals(null, daoUser.selectById(5));
+    }
+
+    @Test
+    public void testUpdate() {
+        User testUser = daoUser.selectById(17);
+        testUser.setName("Eliot");
+
+        daoUser.update(testUser);
+
+        assertEquals("Eliot", daoUser.selectById(17).getName());
+    }
+
+    @Test
+    public void testSelectByUserGroup() throws Exception {
+        //TODO: Group parameter is passed
+    }
+
+    @Test
+    public void testSelectByUserGroupType() throws Exception {
+        //TODO: String parameter is passed
+    }
+
+    @Test
+    public void testExist() throws Exception {
+        //user exist if email is in the db
+        assertTrue(daoUser.exist("email@gmail.com"));
     }
 }
