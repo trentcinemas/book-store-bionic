681ae6a35e8120b7843d36352b5f97f6de3789ac
Evgeniy Baranuk <evgeniy.baranuk@gmail.com>
2014-07-24 16:38:40 +0300

    Guest reply

---
+++ src/com.bionic.book-store/java/PublisherReply.java
@@ -0,0 +1,20 @@
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+/**
+ * Created by jsarafajr on 24.07.14.
+ */
+
+@Path("reply")
+public class PublisherReply {
+
+    @POST
+    @Path("send")
+    public void sendReplyMessage(@FormParam("email") String email,
+                                 @FormParam("receiver") String receiver,
+                                 @FormParam("text") String text) {
+
+
+    }
+}
---
+++ src/com.bionic.book-store/java/entities/ReplyMessage.java
@@ -0,0 +1,95 @@
+package entities;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+/**
+ * Created by jsarafajr on 24.07.14.
+ */
+@Entity
+public class ReplyMessage {
+    private int messageId;
+    private String email;
+    private String text;
+    private Timestamp date;
+    private Byte removed;
+
+    @Id
+    @Column(name = "messageId")
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    @Basic
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "text")
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Basic
+    @Column(name = "date")
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
+    }
+
+    @Basic
+    @Column(name = "removed")
+    public Byte getRemoved() {
+        return removed;
+    }
+
+    public void setRemoved(Byte removed) {
+        this.removed = removed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReplyMessage that = (ReplyMessage) o;
+
+        if (messageId != that.messageId) return false;
+        if (date != null ? !date.equals(that.date) : that.date != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        if (removed != null ? !removed.equals(that.removed) : that.removed != null) return false;
+        if (text != null ? !text.equals(that.text) : that.text != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = messageId;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (text != null ? text.hashCode() : 0);
+        result = 31 * result + (date != null ? date.hashCode() : 0);
+        result = 31 * result + (removed != null ? removed.hashCode() : 0);
+        return result;
+    }
+}
