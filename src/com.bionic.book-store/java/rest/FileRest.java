aae03b6df5e3b874f395923fa359561bff0ffe45
bbetter <fakelessjohn@gmail.com>
2014-07-29 20:45:58 +0300

    dynamic book loading , search is working, added big small cover into addbook

---
+++ src/com.bionic.book-store/java/rest/FileRest.java
@@ -0,0 +1,32 @@
+package rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+import java.io.File;
+
+/**
+ * Created by Джон on 29.07.2014.
+ */
+@Path("file")
+public class FileRest {
+    private static final String FILE_BOOK_PATH="D:/Bionic/Books/";
+
+    @GET
+    @Path("getimage/{path:.+}")
+    @Produces("image/png")
+    public Response getImage(@PathParam("path")String relativeFilePath) {
+
+        File file = new File(FILE_BOOK_PATH+relativeFilePath);
+
+        //Response.ResponseBuilder response = Response.ok((Object) file);
+        /*response.header("Content-Disposition",
+                "attachment; filename=image_from_server.png");
+        */return Response.ok(file).build();
+
+    }
+
+}
