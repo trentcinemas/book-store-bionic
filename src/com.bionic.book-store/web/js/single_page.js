be43aa94b2ab1cd50cc3158025ada68b5a0fc45e
Evgeniy Baranuk <evgeniy.baranuk@gmail.com>
2014-08-06 22:50:06 +0300

    Single page: book preview, comments for unlogined user

--- src/com.bionic.book-store/web/js/single_page.js
+++ src/com.bionic.book-store/web/js/single_page.js
@@ -1,6 +1,8 @@
 /**
  * Created by Джон on 31.07.2014.
  */
+var currentBook;
+
 $(document).ready(function(){
     var URL = window.location.search;
     var getRequest=URL.split("?")[1];
@@ -14,13 +16,14 @@ $(document).ready(function(){
            if(data.newb==true){
                $('.b-badge').css("display","block");
            }
-
+            currentBook = data;
             $("#athor").html(data.author);
             $("#title").html(data.title);
             $("#description").html(data.description);
             $("#price").html(data.price + " грн");
             $("#big-cover").attr("src","/rest/file/getimage/"+data.big_cover);
        }
+
     });
 
     getComments(id);
@@ -43,6 +46,7 @@ $(document).ready(function(){
     addAuthorization();
     formReplyActionSet();
     addBuyBookAction();
+    popupReviewWindow();
 
 });
 
@@ -108,8 +112,10 @@ $('#navbarHeader').ready(function() {
         success: function (data) {
             if (data.name == null) { // if response doesn't have user
                 loginButtonEnable();
+                sendCommentsDisable();
             } else {
                 logoutButtonEnable(data.name);
+                sendCommentsEnabe();
             }
         }
     });
@@ -179,8 +185,25 @@ function formReplyActionSet() {
     });
 }
 
-    function addComment(id,comment)
-    {
+function sendCommentsEnabe() {
+    $('#send_comment_div').html(
+            "<h3 class='page-header'>Додати коментар</h3>" +
+            "<form role='form' id='add_comment_form' method='post'>" +
+            "<input type='hidden' id='book' name='book'>" +
+            "<div class='form-group'>" +
+                "<label for='comment'>Коментар</label>" +
+                "<input type='text' class='form-control' id='comment' name='comment'>" +
+                "</div>" +
+                "<button type='submit' class='btn btn-default' id='submit'>Зберегти</button>" +
+            "</form>"
+    );
+}
+
+function sendCommentsDisable() {
+    $('#send_comment_div').html("");
+}
+
+    function addComment(id,comment) {
 //        $("#add_comment_form").submit(function () {
 
 //            var URL = window.location.search;
@@ -189,7 +212,7 @@ function formReplyActionSet() {
 
             $.ajax({
                 type: "post",
-                url: "rest/comment/addComment",
+                url: "/rest/comment/addComment",
                 data: {
                     book: id,
                     comment: comment
@@ -231,4 +254,84 @@ function formReplyActionSet() {
 
 
 
+/* Pop Up window "REVIEW" */
 
+function popupReviewWindow() {
+    $('body').append('<div class="popup-box" id="popup-box-1">' +
+        '<div class="top">' +
+        "<span id='popup_title' style='font-weight: bold; width: 100%'></span><br>" +
+        "<embed src='/rest/file/getreviewfile/" + id + "' width='100%' height='"+ ($(window).height() / 1.3) + "'>" +
+        '</div>' +
+        '</div>');
+    $('body').append('<div id="blackout"></div>');
+
+    boxWidth = $(window).width() / 1.8;
+    console.log(boxWidth);
+
+    $(window).resize(centerBox);
+    $(window).scroll(centerBox);
+    centerBox();
+
+    $('[class*=popup-link]').click(function(e) {
+
+        /* Prevent default actions */
+        e.preventDefault();
+        e.stopPropagation();
+
+        /* Set popup title */
+        $('#popup_title').html($('#title').html());
+
+        /* Get the id (the number appended to the end of the classes) */
+        var name = $(this).attr('class');
+        var id = name[name.length - 1];
+        var scrollPos = $(window).scrollTop();
+
+        /* Show the correct popup box, show the blackout and disable scrolling */
+        $('#popup-box-'+id).show();
+        $('#blackout').show();
+        $('html,body').css('overflow', 'hidden');
+
+        /* Fixes a bug in Firefox */
+        $('html').scrollTop(scrollPos);
+    });
+    $('[class*=popup-box]').click(function(e) {
+        /* Stop the link working normally on click if it's linked to a popup */
+        e.stopPropagation();
+    });
+    $('html').click(function() {
+        var scrollPos = $(window).scrollTop();
+        /* Hide the popup and blackout when clicking outside the popup */
+        $('[id^=popup-box-]').hide();
+        $('#blackout').hide();
+        $("html,body").css("overflow","auto");
+        $('html').scrollTop(scrollPos);
+    });
+    $('.close').click(function() {
+        var scrollPos = $(window).scrollTop();
+        /* Similarly, hide the popup and blackout when the user clicks close */
+        $('[id^=popup-box-]').hide();
+        $('#blackout').hide();
+        $("html,body").css("overflow","auto");
+        $('html').scrollTop(scrollPos);
+    });
+}
+
+function centerBox() {
+
+    /* Preliminary information */
+    var winWidth = $(window).width();
+    var winHeight = $(document).height();
+    var scrollPos = $(window).scrollTop();
+    /* auto scroll bug */
+
+    /* Calculate positions */
+
+    var disWidth = (winWidth - boxWidth) / 2;
+    var disHeight = scrollPos + 50;
+
+    /* Move stuff about */
+    $('.popup-box').css({'width' : boxWidth+'px', 'left' : disWidth+'px', 'top' : disHeight+'px'});
+    $('#blackout').css({'width' : winWidth+'px', 'height' : winHeight+'px'});
+
+    return false;
+}
